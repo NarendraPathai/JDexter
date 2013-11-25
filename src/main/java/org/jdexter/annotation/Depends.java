3490aa33541a57cf0d4e905d4b88747d7889ac1c
Narendra Pathai <narendra.pathai@gmail.com>
2013-11-25 21:05:42 +0530

    Javadoc for Depends

--- src/main/java/org/jdexter/annotation/Depends.java
+++ src/main/java/org/jdexter/annotation/Depends.java
@@ -5,6 +5,17 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation when specified for a configuration the framework will lookup if the configuration
+ * is already read and cached, if the configuration is already read then the <b><i>same instance</i></b>
+ * is returned, while if configuration is not read then it is freshly read and cached to be provided
+ * for future dependencies. This behavior is different from {@link Configuration} in which every time
+ * a new instance is created and returned.
+ * 
+ * @author Narendra
+ *
+ */
+
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Depends {
